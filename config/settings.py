30e05fb4f586181723763fb20198dadd6e137f53
Mahdi <mahdighadami2@gmail.com>
2023-06-25 21:54:33 +0330

    fix connection between mySQL & django

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {  
     'default': {  
         'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'djangodb',  
+        'NAME': 'mysql',  
         'USER':'root',  
         'PASSWORD':'',  
         'HOST':'localhost',  
