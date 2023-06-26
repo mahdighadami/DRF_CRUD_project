dc6bdad799065d1ce91b75707e04497950e68805
Mahdi <mahdighadami2@gmail.com>
2023-06-26 20:45:06 +0330

    change view for handling new form

--- student/views.py
+++ student/views.py
@@ -1,5 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .models import Student
+from .forms import StudentForm
 
 # Create your views here.
-def home(request):
-    return render(request, "home.html")
+
+def create(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("/thanks/")
+    else:
+        form = StudentForm()
+    return render(request, "home.html", {"form" : form})
