7fe68a93d99aaa414b6f213c3458b353f608b098
Mahdi <mahdighadami2@gmail.com>
2023-06-26 22:19:07 +0330

    editing form and HTML page

--- student/forms.py
+++ student/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from phonenumber_field.formfields import PhoneNumberField
-
+from .models import Student
+from django.forms import ModelForm
 
 
 class StudentForm(forms.Form):
@@ -19,9 +20,9 @@ class StudentForm(forms.Form):
     last_name = forms.CharField(label="Last Name", max_length=50)
     sex = forms.ChoiceField(label="Sex", choices=SEX_CHOICES)
     grade = forms.ChoiceField(label="Grade", choices=GRADE_CHOICES)
-    birth_date = forms.DateTimeField(label="Birth", widget=forms.SelectDateWidget)
-    register_date = forms.DateTimeField(label="Register Date", widget=forms.SelectDateWidget)
-    graduation_date = forms.DateTimeField(label="Graduation Date", widget=forms.SelectDateWidget)
+    birth_date = forms.DateField(label="Birth", widget=forms.SelectDateWidget(years = range(1970,2010)))
+    register_date = forms.DateTimeField(label="Register Date", widget=forms.SelectDateWidget(years = range(1970,2024)))
+    graduation_date = forms.DateTimeField(label="Graduation Date", widget=forms.SelectDateWidget(years = range(1970,2024)))
     address = forms.CharField(label="Address", widget=forms.Textarea)
     phone = PhoneNumberField()
 
