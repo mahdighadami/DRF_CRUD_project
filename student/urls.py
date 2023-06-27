a939a01feaa92190ccfa8d6c7cf7036aa4d3a6c7
Mahdi <mahdighadami2@gmail.com>
2023-06-27 22:45:34 +0330

    update urls

--- student/urls.py
+++ student/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import home
 from .views import create
 from .views import read
+from .views import update
 
 
 app_name = "student"
 urlpatterns = [
     path('', home, name="home"),
     path('create', create, name="create"),
-    path('read', read, name="read")
-
+    path('read', read, name="read"),
+    path('update', update, name="update")
 
 ]
