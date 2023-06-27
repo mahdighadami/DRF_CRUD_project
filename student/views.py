432b61dbea6d39e7afc29b14df02bd91324f2d4a
Mahdi <mahdighadami2@gmail.com>
2023-06-27 13:02:36 +0330

    add new methods

--- student/views.py
+++ student/views.py
@@ -1,15 +1,29 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Student
 from .forms import StudentForm
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
 
 def create(request):
+    context = {}
     if request.method == "POST":
         form = StudentForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect("/thanks/")
+            context['created'] = True
+            context['form'] = form
+            return render(request, "create.html", context = context)
+        else:
+            context['created'] = False
+            context['form'] = form
+            return render(request, "create.html", context = context)
+
     else:
         form = StudentForm()
-    return render(request, "home.html", {"form" : form})
+        context['created'] = False
+        context['form'] = form
+    return render(request, "create.html", context = context)
+
+def read(request):
+    pass
