ed4d75c6d91dd8a5faea2409367cca6fbe884f7d
Mahdi <mahdighadami2@gmail.com>
2023-06-27 13:00:39 +0330

    add new urls

--- student/urls.py
+++ student/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import home
 from .views import create
+from .views import read
+
 
 app_name = "student"
 urlpatterns = [
-    path('', create, name="create")
+    path('', home, name="home"),
+    path('create', create, name="create"),
+    path('read', read, name="read")
+
 
 ]
