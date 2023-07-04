7d90f2200eada81a76ee809d0a76b2cec2f34622
Mahdi <mahdighadami2@gmail.com>
2023-07-04 23:19:45 +0330

    add api urls and change settings

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'phonenumber_field',
-    'student.apps.StudentConfig'
+
+    'student.apps.StudentConfig',
+    'api.apps.ApiConfig',
+
     'rest_framework',
 ]
 
